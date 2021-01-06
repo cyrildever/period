f522cc5a4fee823e808b79376dc986d8a4897c4d
Cyril Dever <cdever@edgewhere.fr>
2021-01-06 12:39:33 +0100

    a few tests

---
+++ period_test.go
@@ -0,0 +1,37 @@
+package period_test
+
+import (
+	"testing"
+
+	"github.com/cyrildever/period"
+	"gotest.tools/assert"
+)
+
+// TestGetDuration ...
+func TestGetDuration(t *testing.T) {
+	var ref int64 = 10000
+	span := uint64(ref)
+	err := period.Init(0, span, true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Assert(t, period.IsTestEnvironment())
+	d := period.GetDuration().Milliseconds()
+	assert.Equal(t, ref, d)
+}
+
+// TestNext ...
+func TestNext(t *testing.T) {
+	err := period.Init(0, 10000, true)
+	if err != nil {
+		t.Fatal(err)
+	}
+	p, err := period.Get(1)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, p.ID, uint64(1)) // First period is 1
+
+	next := p.Next()
+	assert.Equal(t, next.ID, p.ID+1)
+}
